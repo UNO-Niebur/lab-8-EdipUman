9023beb13bf23784fdd4373c414e6b6f7a2a0b79
EdipUman <euman@unomaha.edu>
2026-03-28 12:06:04 -0500

    Completed ProcessData.py

--- ProcessData.py
+++ ProcessData.py
@@ -1,23 +1,66 @@
 #ProcessData.py
-#Name:
-#Date:
-#Assignment:
+#Name:Edip Uman
+#Date:3/28/26
+#Assignment: Lab 8
 
 import random
-
+ 
+def makeID(first, last, idNum):
+    
+    if len(last) < 5:
+        last = last + "x"
+ 
+    
+    idLen = len(idNum)
+    id = first[0].lower() + last.lower() + idNum[idLen - 3:]
+    return id
+ 
+def makeMajorYear(major, year):
+    
+    majorPart = major[:3].upper()
+ 
+    
+    if year == "Freshman":
+        yearPart = "FR"
+    elif year == "Sophomore":
+        yearPart = "SO"
+    elif year == "Junior":
+        yearPart = "JR"
+    else:
+        yearPart = "SR"
+ 
+    return majorPart + "-" + yearPart
+ 
+def getFirst(data):
+    return data[0]
+ 
+def getLast(data):
+    return data[1]
+ 
 def main():
-
-  #Open the files we will be using
-  inFile = open("names.dat", 'r')
-  outFile = open("StudentList.csv", 'w')
-
-  #Process each line of the input file and output to the CSV file
-
-
-
-  #Close files in the end to save and ensure they are not damaged.
-  inFile.close()
-  outFile.close()
-
+ 
+   
+    inFile = open("names.dat", 'r')
+    outFile = open("StudentList.csv", 'w')
+ 
+    
+    for line in inFile:
+        data = line.split()
+        first = getFirst(data)
+        last = getLast(data)
+        idNum = data[3]
+        year = data[5]
+        major = data[6]
+ 
+        student_id = makeID(first, last, idNum)
+        major_year = makeMajorYear(major, year)
+ 
+        output = last + "," + first + "," + student_id + "," + major_year + "\n"
+        outFile.write(output)
+ 
+    #Close files in the end to save and ensure they are not damaged.
+    inFile.close()
+    outFile.close()
+ 
 if __name__ == '__main__':
-  main()
+    main()
